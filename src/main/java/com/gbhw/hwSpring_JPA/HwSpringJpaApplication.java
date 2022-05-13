915324a778d47412233a8fe550de85f158c1b15b
lasteik <lastevilday@gmail.com>
2022-05-13 10:40:30 +0300

    Initial commit

---
+++ src/main/java/com/gbhw/hwSpring_JPA/HwSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.gbhw.hwSpring_JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwSpringJpaApplication.class, args);
+	}
+
+}
